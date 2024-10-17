bca6064ebfb383dffab31ba869f0d992c7d8435e
sanjeev567485463 <nk973711@gmail.com>
2024-10-17 06:26:31 +0530

    all file deleted

--- resources/views/frontend/layouts/master.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('frontend.layouts.base')
-@section('base.content')
-    @include('frontend.layouts.header.' . $section['header'])
-    @yield('content')
-    @include('frontend.homePage.cta')
-    @include('frontend.layouts.footer')
-@endsection
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use Illuminate\Support\Facades\Route;
-
-Route::fallback(function () {
-    return redirect()->route('install.initialize');
-});
-
